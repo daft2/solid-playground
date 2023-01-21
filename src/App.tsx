ca72450205d702174109e7cff5ca0a1dbb2e0bb4
Daft <daftdevs@gmail.com>
2023-01-21 21:50:35 +0700

    feat: display counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import type { Component } from "solid-js";
+import { Component } from "solid-js";
+import Counter from "./components/Counter";
 
 const App: Component = () => {
   return (
-    <div class="bg-neutral-800 text-neutral-100 w-screen h-screen">
-      <div>
-        <p>Hello Solid</p>
-      </div>
+    <div class="bg-neutral-800 text-neutral-100 w-screen h-screen items-center justify-center flex flex-col">
+      <Counter />
     </div>
   );
 };
