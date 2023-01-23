b901d260314218727671d226640c9ef1ad497519
Daft <daftdevs@gmail.com>
2023-01-23 16:09:47 +0700

    feat: add & get value from localstorage test

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,22 +1,35 @@
-import { Component, For } from "solid-js";
-
-const MOCK_CARD_ITEM = [
-  { id: 1, value: "Item 1" },
-  { id: 2, value: "Item 2" },
-  { id: 3, value: "Item 3" },
-];
+import { Component, createEffect, createSignal, For } from "solid-js";
 
 const KanbanBoard: Component = (props) => {
+  const [items, setItems] = createSignal<string[]>([]);
+
+  const addItem = (value: string) => {
+    const newData = [...items(), value];
+    localStorage.setItem("kanbanBoard", JSON.stringify(newData));
+    setItems(newData);
+  };
+
+  createEffect(() => {
+    const data = localStorage.getItem("kanbanBoard");
+
+    if (data !== null) setItems(JSON.parse(data));
+  });
+
   return (
     <div class="flex gap-2 w-full h-screen overflow-hidden">
       <div class="bg-neutral-900 text-gray-200 w-full max-w-[15rem] h-fit p-2">
         <p class="text-sm">Card Name</p>
         <div class="flex flex-col gap-2 my-3 max-h-[50rem] overflow-y-auto">
-          <For each={MOCK_CARD_ITEM}>
-            {(item, index) => <div class="bg-black/20 p-2">{item.value}</div>}
+          <For each={items()}>
+            {(item, index) => <div class="bg-black/20 p-2">{item}</div>}
           </For>
         </div>
-        <button class="bg-black/20 p-2 w-full">Add new item</button>
+        <button
+          class="bg-black/20 p-2 w-full"
+          onClick={() => addItem("Item 1")}
+        >
+          Add new item
+        </button>
       </div>
     </div>
   );
